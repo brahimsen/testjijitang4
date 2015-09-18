069c9d1d13cea0ce1f7f3ebabb9c125a53f5ac49
brahim <brahim.senhaji@gmail.com>
2015-09-18 11:37:35 -0700

    Please find the new folder mobile-app. Inside all files related to new first version. I dont wanna let you wait I committed now the welcome page and complete login form with validation. in few minutes I will follow it up with the complete form for sign up with validation also. Later I will commit the user registration form also with validation. Xu please find welcome-page.html and login.html and login-form-validation.js. these files are complete and you can start working on them now.

---
+++ Mobile-App/js/follow-script.js
@@ -0,0 +1,14 @@
+ $(document).ready(function() {
+	 
+	 $(".unfollow-btn").hide();
+	 $(".follow-user-btn-holder").click(function(){
+						$(".follow-btn,.unfollow-btn",this).toggle(
+						
+						
+						);
+					
+						
+								
+				});
+				
+			});
---
+++ Mobile-App/js/mobile-script.js
@@ -0,0 +1,7 @@
+    $(document).ready(function(){
+			
+		       var myDiv = $('.article-update-text');
+				myDiv.text(myDiv.text().substring(0,110) + '...')
+        
+        });
+        
