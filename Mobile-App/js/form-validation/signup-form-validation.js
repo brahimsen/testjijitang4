9a6f03bb0d6812ac6331bef41eec955712086848
brahim <brahim.senhaji@gmail.com>
2015-09-18 14:44:48 -0700

    Please find the signup.html and form-validation/signup-form-validation.js for sign up form and its validation

--- Mobile-App/js/form-validation/signup-form-validation.js
+++ Mobile-App/js/form-validation/signup-form-validation.js
@@ -1,32 +1,94 @@
-
-
 $(document).ready(function(){
-	
-$('form').each(function(key, form) {
-$(".phone-number-form").validate(
-{rules:
-{phonenumber:"required",
+		var current = 1;
+		
+		widget      = $(".step");
+		btnnext     = $(".next");
+		btnback     = $(".back"); 
+		btnsubmit   = $(".submit");
 
-},
+		// Init buttons and UI
+		widget.not(':eq(0)').hide();
+		hideButtons(current);
+		
 
-	 
+		// Next button click action
+		btnnext.click(function(){
+			if(current < widget.length){
+				// Check validation
+				if($(".form").valid()){
+					widget.show();
+					widget.not(':eq('+(current++)+')').hide();
+					
+				}
+			}
+			hideButtons(current);
+		})
+		
+		btnsubmit.click(function(){
+		
+			if($(".form").valid()){
+					
+					alert("Its up you now xu");
+					
+				}
+			
+		})
 
-   invalidHandler: function(form, validator) {
-            var errors = validator.numberOfInvalids();
+		// Back button click action
+		btnback.click(function(){
+			if(current > 1){
+				current = current - 2;
+				if(current < widget.length){
+					widget.show();
+					widget.not(':eq('+(current++)+')').hide();
+					
+				}
+			}
+			hideButtons(current);
+		})
 
-            if (errors) {
-                $("#phone-error-message").show().text("Please Enter Phone Number");
-            } else {
-                $("#phone-error-message").hide();
-            }
-        },
+	    $('.form').validate({ // initialize plugin
+			ignore:":not(:visible)",			
+			rules: {
+				phonenumber : "required",
+				verfification : "required",
+				fpassword : "required",
+				rpassword: { required : true, equalTo: "#fpassword"},
+				
+				
+			
+			},
+			
+			messages: {
+				phonenumber : "Please Enter Phone Number",
+				verfification:"Please Enter Code",
+				fpassword:"Please Enter Password",
+				rpassword: {
+					required: "Please Confirm your password",
+					equalTo: "Password does not mutch"
+				},
+			},
+			
+			errorPlacement: function(error, element) {
+				 error.appendTo($('#sign-up-error'));
+			}
+	    });
 
-	errorPlacement: function(error, element){}
-	
-}
-  
-);
+	});
 
-);
 
-});
+	// Hide buttons according to the current step
+	hideButtons = function(current){
+		var limit = parseInt(widget.length); 
+
+		$(".action").hide();
+
+		if(current < limit) btnnext.show();
+		if(current > 1) btnback.show();
+		if (current == limit) { 
+			// Show entered values
+			
+			btnnext.hide(); 
+			btnsubmit.show();
+		}
+	}
