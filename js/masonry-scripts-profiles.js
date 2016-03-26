9d3e70511bfa0093351ac0077242288003c6c022
brahim <brahim.senhaji@gmail.com>
2016-03-25 18:13:27 -0700

    the issue of update ranks fixed

---
+++ js/masonry-scripts-profiles.js
@@ -0,0 +1,21 @@
+$('.collection-columns').masonry({
+  itemSelector: '.collection-grid-item',
+  percentPosition: true,
+  gutter: 20
+});
+
+$('#project-columns').masonry({
+  itemSelector: '.project-square-grid-item',
+  percentPosition: true,
+  gutter: 20
+});
+$('.profile-columns').masonry({
+  itemSelector: '.profile-grid-item',
+  percentPosition: true,
+  gutter: 20
+});
+$('.article-columns').masonry({
+  itemSelector: '.square-grid-item',
+  percentPosition: true,
+  gutter: 20
+});
