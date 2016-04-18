60d48c73219cb7350e3647a21d96595a9e2cec17
brahim <brahim.senhaji@gmail.com>
2016-04-18 14:50:37 -0700

    my-projects finished

---
+++ js/my-projects-top-scripts.js
@@ -0,0 +1,58 @@
+
+$(document).ready(function(){
+	
+
+	
+	
+	$("#add-interests-btn").click(function(e) {
+    
+           $(".project-keyword-list").append(" <div class='editor-all-tags'><span class='tag-text'>"+$("#project-keywords").val()+"</span><div class='tag-delete lnr lnr-cross'></div></div>");
+	   $("#project-keywords").val('')
+   
+});
+
+	 $('.project-keyword-list').on('click', '.tag-delete', function(){
+	 	
+	 $(this).parent().remove()
+   
+});
+	
+	
+	});
+
+ $(document).ready(function() {
+var options = {
+  valueNames: [ 'name' ]
+};
+
+var userList = new List('my-projects', options);
+
+});
+ $(document).ready(function() {
+$("#create-project-btn").click(function(){
+				
+				$("#create-project-modal").modal('show');
+
+
+		});
+		
+		
+	
+		});
+		
+				$(function() {
+
+    $( "#datepickerstart" ).datepicker({
+      changeMonth: true,
+      changeYear: true,
+	  yearRange: "1985:2045"
+    });
+	
+	 $( "#datepickerend" ).datepicker({
+      changeMonth: true,
+      changeYear: true,
+	  yearRange: "1985:2045"
+    });
+  });
+		
+	
--- js/project-form.js
+++ js/project-form.js
@@ -1,54 +1,112 @@
-
-
 $(document).ready(function(){
+		var current = 1;
+		
+		widget      = $(".step");
+		btnnext     = $(".next");
+		btnback     = $(".back"); 
+		btnsubmit   = $(".submit");
 
+		// Init buttons and UI
+		widget.not(':eq(0)').hide();
+		hideButtons(current);
+		
 
+		// Next button click action
+		btnnext.click(function(){
+			if(current < widget.length){
+				// Check validation
+				if($(".form").valid()){
+					widget.show();
+					widget.not(':eq('+(current++)+')').hide();
+					
+				}
+			}
+			hideButtons(current);
+		})
+		
+		
+		// if form valid go to user infomation
+		
+		btnsubmit.click(function(){
+		
+			if($(".form").valid()){
+					
+					window.location.href = "my-projects.html";
+					
+				}
+			
+		})
 
-$(".project-form-main").validate(
-{rules:
-{name:"required",
-projecttyperadio:{required:true},
-datepickerstart:{required:true},
-datepickerend:{required:true},
+		// Back button click action
+		btnback.click(function(){
+			if(current > 1){
+				current = current - 2;
+				if(current < widget.length){
+					widget.show();
+					widget.not(':eq('+(current++)+')').hide();
+					
+				}
+			}
+			hideButtons(current);
+		})
 
-email:{required:true,email:true},
-pass: {required:true, minlength: 6},
-passconf: {required:true, equalTo: "#pass"},
-},
-
-messages: {
-
-name:"Please write down your name",
-email: {
-required: "Please enter your Email",
-email: "Please Enter a valid Email address"
-},
-pass: {
-required: "Please enter your password",
-minlength: "Password must be at least 6 characters"
-},
-passconf: {
-required: "Please enter your password again",
-equalTo: "Password does not mutch"
-},
-
-
-},
-errorClass:"error",
-highlight: function(label) {
-    $(label).closest('.form-group').removeClass('has-success').addClass('has-error');
-  },
+	    $('.form').validate({ // initialize plugin
+			ignore:":not(:visible)",			
+			rules: {
+				projecttitle:"required",
+				datepickerstart:"required",
+				datepickerend:"required",
+				
+				
+			
+			},
+			
+			messages: {
+				projecttitle:"Please write down project title",
+				datepickerstart:"Please select starting date",
+				datepickerend:"Please select ending date",
+			},
+			
+			
+				errorClass:"error",
+				highlight: function(label) {
+				$(label).closest('.form-group').removeClass('has-success').addClass('has-error');
+				},
 
   
-success: function(label) {
-    label
-      .text('looks good').addClass('valid')
-	  .closest('.form-group').addClass('has-success');
+				success: function(label) {
+					label
+				.text('looks good').addClass('valid')
+				.closest('.form-group').addClass('has-success');
       
-  }
-}
-  
-);
+				}
+			
+			
+			
+			
+			
+			
+	    });
 
-});
+	});
+
+
+	// Hide buttons according to the current step
+	hideButtons = function(current){
+		var limit = parseInt(widget.length); 
+
+		$(".action").hide();
+
+		if(current < limit) btnnext.show();
+		if(current > 1) btnback.show();
+		if (current == limit) { 
+			// Show entered values
+			
+			btnnext.hide(); 
+			btnsubmit.show();
+		}
+	}
+	
+
+	
 
